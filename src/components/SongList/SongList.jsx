1851aa2b88934707fb616c3bc969588c6dc3e472
Joey <30815220+joeylking@users.noreply.github.com>
2021-11-18 10:42:54 -0600

    Put SongList into a table

--- src/components/SongList/SongList.jsx
+++ src/components/SongList/SongList.jsx
@@ -14,13 +14,30 @@ const SongList = () => {
   }, []);
   console.log(songs);
   return (
-    <>
-      {songs.map(song => (
-        <h1>
-          {song.title} - {song.artist}
-        </h1>
-      ))}
-    </>
+    <table>
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Artist</th>
+          <th>Album</th>
+          <th>Genre</th>
+          <th>Release Date</th>
+        </tr>
+      </thead>
+      <tbody>
+        {songs.map(song => (
+          <tr key={song.id}>
+            <td>{song.title}</td>
+            <td>{song.artist}</td>
+            <td>{song.album}</td>
+            <td>{song.genre}</td>
+            <td>{song.release_date}</td>
+            <button>Edit</button>
+            <button>Delete</button>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 };
 
